4c575bd0c76ee7b7c8367f3538171a81f3bdf549
Dhruvsingh121 <GodZeusOP@gmail.com>
2025-03-03 01:07:54 +0530

    Recipe-Fetched

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ const recipeContainer = document.querySelector(".recipe-container");
 
 // function to get recipies
 const fetchRecipies = async (query) => {
+  recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";
   const data = await fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
   );
   const response = await data.json();
-  console.log(response);
+
+  recipeContainer.innerHTML = "";
+  response.meals.forEach((meal) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `
+    <img src ="${meal.strMealThumb}">
+    <h3>${meal.strMeal}</h3>
+    <p>${meal.strArea} Dish</p>
+    <p>${meal.strCategory}</p>
+    `;
+    const button = document.createElement("button");
+    button.textContent = "View Recipe";
+    recipeDiv.appendChild(button);
+    // button.addEventListener("click",()=>{
+
+    // })
+    recipeContainer.appendChild(recipeDiv);
+  });
 };
 
 searchBtn.addEventListener("click", (e) => {
